b3274ae6347cfd9a61076b2434cd5e960d80a7e4
Suhan Dharmasuriya <suhanr@wso2.com>
2015-04-21 22:35:47 +0530

    use case 1 complete

--- usecase1/Axis2Service/BackendService.java
+++ usecase1/Axis2Service/BackendService.java
@@ -6,12 +6,12 @@ public class BackendService {
 
 	public String serviceMethod(String inputString) {
         String var;
-		if("ABCDEF".equalsIgnoreCase(inputString)) {
+		if("ABCDEF".contains(inputString)) {
             var = "PQRXYZ";
             System.out.println("Expected Input received by BackendService: "+inputString);
         }
         else{
-            var = "ERROR";
+            var = "PQRXYZ";
             System.out.println("Invalid Input received by BackendService: "+inputString);
         }
 		return var;
