a2d827d21f2d4f0873cf142fdb53d1b2662798af
Suhan Dharmasuriya <suhand@users.noreply.github.com>
2015-05-02 10:05:40 +0530

    Update script.sql

--- usecase3/MySQL/script.sql
+++ usecase3/MySQL/script.sql
@@ -1,7 +1,3 @@
 create database dss_sample;
 use dss_sample;
 CREATE TABLE Employee(EmployeeID int PRIMARY KEY AUTO_INCREMENT, FirstName varchar(255), LastName varchar(255),Team varchar(255));
-
-create database us_office;
-use us_office;
-CREATE TABLE Employee(EmployeeID int PRIMARY KEY AUTO_INCREMENT, FirstName varchar(255), LastName varchar(255),Department varchar(255));
